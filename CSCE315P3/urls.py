53e1882e34ab43c10bd3af053bb4bd3ffbe84cf2
SunheeEllisseKim <56172905+SunheeEllisseKim@users.noreply.github.com>
2022-04-05 13:00:17 -0500

    Update urls.py
    edited urls.py in CSCE315P3 folder- deployment issue using just v2 food bank import

--- CSCE315P3/urls.py
+++ CSCE315P3/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
-from post import views as v1
-#from foodbank import views as v2
+#from post import views as v1
+from foodbank import views as v2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('post/', v1.index, name='index'),
-    path('foodbank/', v1.index, name='index')
+    path('post/', v2.index, name='index'),
+    path('foodbank/', v2.index, name='index')
 ]
