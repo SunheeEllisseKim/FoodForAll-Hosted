2a4140a6846d4dcd8ac0197fea287edd48fbd049
ETingRice <ethanting888@gmail.com>
2022-04-12 21:53:30 -0500

    Small Change

--- forms/views.py
+++ forms/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 # Create your views here.
+
+def get_food(request):
+    if request.method == 'POST':
+        content = request.POST.get('content', '')
+
+        if content:
+            print('Content', content)
+
+            return redirect('index')
+
+    return render(request,'')
